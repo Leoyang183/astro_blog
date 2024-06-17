638f354a4be867c7a037d6329093b51926bdd5ed
chenghsien <chenghsien.yang@taxigo.com.tw>
2024-06-18 01:09:46 +0800

    feat: add custom domain setting

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leoyang183.github.io',
+    site: 'blog.leoyang183.com',
     base: '',
   })
