8b89f901b3d251d6047978905a7350bf975527d4
leoyang <wadxmjh@gmail.com>
2024-06-30 00:49:27 +0800

    feat: astro-paper hello world

---
+++ astro-paper/src/config.ts
@@ -0,0 +1,147 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://blog.leoyang183.com", // replace this with your deployed domain
+  author: "Leo Yang",
+  desc: "楊承憲的部落格",
+  title: "楊承憲的部落格",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "tw", // html lang code. Set this empty and default will be "en"
+  langTag: ["zh-TW"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/leoyang183",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  // {
+  //   name: "Facebook",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Facebook`,
+  //   active: true,
+  // },
+  // {
+  //   name: "Instagram",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Instagram`,
+  //   active: true,
+  // },
+  {
+    name: "LinkedIn",
+    href: "https://portaly.cc/leoyang183",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:wadxmjh@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  // {
+  //   name: "Twitter",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Twitter`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Twitch",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Twitch`,
+  //   active: false,
+  // },
+  // {
+  //   name: "YouTube",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on YouTube`,
+  //   active: false,
+  // },
+  // {
+  //   name: "WhatsApp",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on WhatsApp`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Snapchat",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Snapchat`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Pinterest",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Pinterest`,
+  //   active: false,
+  // },
+  // {
+  //   name: "TikTok",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on TikTok`,
+  //   active: false,
+  // },
+  // {
+  //   name: "CodePen",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on CodePen`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Discord",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Discord`,
+  //   active: false,
+  // },
+  // {
+  //   name: "GitLab",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on GitLab`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Reddit",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Reddit`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Skype",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Skype`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Steam",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Steam`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Telegram",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Telegram`,
+  //   active: false,
+  // },
+  // {
+  //   name: "Mastodon",
+  //   href: "https://github.com/satnaing/astro-paper",
+  //   linkTitle: `${SITE.title} on Mastodon`,
+  //   active: false,
+  // },
+];
